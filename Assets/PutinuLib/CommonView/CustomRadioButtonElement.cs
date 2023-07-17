6bfb67c8be16c99292903ff7ce72693ea4801453
putinu <putinu2521@gmail.com>
2023-07-17 21:30:39 +0900

    [add] ラジオボタンの各要素ボタンのクラス作成

---
+++ Assets/PutinuLib/CommonView/CustomRadioButtonElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace PutinuLib.CommonView
+{
+    /// <summary>
+    /// ラジオボタンの各要素クラス
+    /// </summary>
+    public class CustomRadioButtonElement : CustomButton
+    {
+        /// <summary>
+        /// 選択されたとき
+        /// </summary>
+        public IObservable<Unit> OnSelected => _selectedSubject;
+        
+        /// <summary>
+        /// 選択が解除されたとき
+        /// </summary>
+        public IObservable<Unit> OnUnselected => _unselectedSubject;
+        
+        private readonly Subject<Unit> _selectedSubject = new();
+        private readonly Subject<Unit> _unselectedSubject = new();
+
+        private bool _isSelected;
+
+        private void OnDestroy()
+        {
+            Debug.Log("[CustomRadioButtonElement]");
+            _selectedSubject.Dispose();
+        }
+
+        /// <summary>
+        /// 選択状態をセットする
+        /// </summary>
+        public void SetSelected(bool isSelected)
+        {
+            if (isSelected == _isSelected) return;
+
+            if (isSelected)
+            {
+                _selectedSubject?.OnNext(Unit.Default);
+            }
+            else
+            {
+                _unselectedSubject?.OnNext(Unit.Default);
+            }
+
+            _isSelected = isSelected;
+        }
+    }
+}
