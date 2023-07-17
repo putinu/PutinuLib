1ad0de2d6d47eed0f439c045fcdd057495425a40
putinu <putinu2521@gmail.com>
2023-07-17 21:30:21 +0900

    [add] UIMonoBehaviourの抽象化

--- Assets/PutinuLib/CommonView/Base/UIMonoBehaviour.cs
+++ Assets/PutinuLib/CommonView/Base/UIMonoBehaviour.cs
@@ -5,7 +5,7 @@ namespace PutinuLib.CommonView
     /// <summary>
     /// UIであるMonoBehaviourが継承して使う
     /// </summary>
-    public class UIMonoBehaviour : MonoBehaviour
+    public abstract class UIMonoBehaviour : MonoBehaviour
     {
         public RectTransform RectTransform => transform as RectTransform;
     }
