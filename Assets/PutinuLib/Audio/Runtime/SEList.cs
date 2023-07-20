a2d506bdc8f99417df3225b430b21378a3d424ea
putinu <putinu2521@gmail.com>
2023-07-20 15:21:37 +0900

    [add] SEListとTypeを追加

---
+++ Assets/PutinuLib/Audio/Runtime/SEList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PutinuLib.Audio
+{
+    /// <summary>
+    /// ScriptableObjectから定義するSEリスト
+    /// </summary>
+    [CreateAssetMenu(menuName = "PutinuLib/Audio/SEList")]
+    public class SEList : ScriptableObject
+    {
+        [SerializeField] private List<AudioClip> _seList;
+
+        /// <summary>
+        /// 対象の種類のAudioClipを取得する
+        /// </summary>
+        /// <param name="seType"></param>
+        /// <returns></returns>
+        public AudioClip GetAudioClip(SEType seType)
+        {
+            int index = (int) seType;
+            return _seList[index];
+        }
+    }
+}
---
+++ Assets/PutinuLib/Audio/Runtime/SEType.cs
@@ -0,0 +1,11 @@
+﻿namespace PutinuLib.Audio
+{
+    /// <summary>
+    /// SEの種類
+    /// エディタ拡張で自動追加される
+    /// </summary>
+    public enum SEType
+    {
+        
+    }
+}
